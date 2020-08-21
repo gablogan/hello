4082bdc59433f0e842ca1a06268a8e2d8345ddfa
gablogan <glogan3@huskers.unl.edu>
2020-08-21 21:58:11 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Gabe Logan
+ * Date: 2020/08/18
+ *
+ * A simple Hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main( int argc,char **argv) {
+
+    printf("Hello World!\n");
+
+    return 0;
+}
